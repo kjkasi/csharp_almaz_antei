9e0f3c349e44c9d760f8314792e04acf32266ed8
root <localhost@localdomain>
2022-02-02 16:21:54 +0300

    Получение числа за наименьшее число шагов

---
+++ 2.Functions_arrays/ShortestStepsToANumber/ShortestStepsToANumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestStepsToANumber
+{
+    class Program
+    {
+        static int Test(int number)
+        {
+            int result = 0;
+            int num = number;
+
+            for (int i = 0; num > 1; i++)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else
+                {
+                    num -= 1;
+                }
+                result += 1;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Test(1);
+            Test(12);
+            Test(16);
+            Test(71);
+            Console.ReadKey();
+        }
+    }
+}
