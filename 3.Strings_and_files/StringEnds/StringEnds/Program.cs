84bcde08e54a776e6ee2f35bbb96d30f7c84b1ba
root <localhost@localdomain>
2022-02-03 10:48:04 +0300

    На что заканчивается строка

---
+++ 3.Strings_and_files/StringEnds/StringEnds/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEnds
+{
+    class Program
+    {
+        static bool Test(string str, string ending)
+        {
+            return str.EndsWith(ending) ? true : false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Test("abc", "bc"));
+            Console.WriteLine(Test("abc", "d"));
+            Console.ReadKey();
+        }
+    }
+}
