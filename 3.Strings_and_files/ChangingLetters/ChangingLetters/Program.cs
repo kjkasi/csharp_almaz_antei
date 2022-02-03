35cbe99a582d110bae04cace27d7226b4d95948d
root <localhost@localdomain>
2022-02-03 10:39:25 +0300

    Изменение символов

---
+++ 3.Strings_and_files/ChangingLetters/ChangingLetters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangingLetters
+{
+    class Program
+    {
+        static string Test(string s)
+        {
+            string result = s;
+            for (int i = 0; i < result.Length; i++)
+            {
+                string str = result.Substring(i, 1);
+                if (str == "a" ||
+                    str == "e" ||
+                    str == "i" ||
+                    str == "o" ||
+                    str == "u")
+                {
+                    string uStr = str.ToUpper();
+                    result = result.Replace(str, uStr);
+                    //result[i] = result[i].;
+                    //Console.WriteLine(result);
+                }
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Test("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
